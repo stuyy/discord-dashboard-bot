90d73a775a32a2c2090b2acf770897c7fab61a83
Anson <ansfoong@gmail.com>
2021-12-30 01:46:50 -0700

    Install TypeORM & Connect to Database

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 require('dotenv').config();
+import 'reflect-metadata';
 import { registerCommands, registerEvents } from './utils/registry';
 import config from '../slappey.json';
 import DiscordClient from './client/client';
 import { Intents } from 'discord.js';
+import { createConnection } from 'typeorm';
 const client = new DiscordClient({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
@@ -12,4 +14,12 @@ const client = new DiscordClient({
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
   await client.login(process.env.DJS_BOT_TOKEN);
+  await createConnection({
+    type: 'mysql',
+    host: process.env.MYSQL_DB_HOST,
+    port: 3306,
+    username: process.env.MYSQL_DB_USERNAME,
+    password: process.env.MYSQL_DB_PASSWORD,
+    database: process.env.MYSQL_DB_DATABASE,
+  });
 })();
